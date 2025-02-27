3f6816a9e275d944e178cff79c8c90bfe48a1baf
Rob <160157653+RobLaran@users.noreply.github.com>
2025-02-27 14:03:30 +0800

    Some changes

--- webpack.prod.js
+++ webpack.prod.js
@@ -6,7 +6,7 @@ const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 module.exports = merge(common, {
     mode: 'production',
     devtool: 'source-map',
-    plugins: [
+    plugins: [  
         new MiniCssExtractPlugin({
           filename: "[name].css",
           chunkFilename: "[id].css",

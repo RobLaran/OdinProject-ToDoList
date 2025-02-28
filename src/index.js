05291689f2c7741cd94d256914c65536f65b19ad
Rob <160157653+RobLaran@users.noreply.github.com>
2025-02-28 16:51:12 +0800

    Starting to create models

--- src/index.js
+++ src/index.js
@@ -1,2 +1,16 @@
 import "./styles.css";
 
+import { createTask, updateTask, taskDetails } from "./modules/models/task.model.js";
+
+const task = createTask();
+
+console.log(task.title);
+
+const taskUpdates = createTask("New title","my description",null,2);
+
+updateTask(task,taskUpdates);
+
+console.log(task.title);
+
+taskDetails(task);
+
---
+++ src/modules/models/task.model.js
@@ -0,0 +1,33 @@
+// Task model
+
+// Functions
+
+// create task
+// input: title, description, dueDate, priority, notes
+export function createTask(title="Untitled task",description="",dueDate=null,priority=1,notes="A note to guide my task") {
+    return {
+        title,
+        description,
+        dueDate,
+        priority,
+        notes
+    }
+};
+
+// update task detail
+export function updateTask(task, taskUpdates) {
+    if(task instanceof Object && taskUpdates instanceof Object) {
+        Object.assign(task,taskUpdates);
+    }
+}
+
+// delete task
+
+// display task detail
+export function taskDetails(task) {
+    if(task instanceof Object) {
+        for(const [key, value] of Object.entries(task)) {
+            console.log(`${key}: ${value}`);
+        }
+    }
+}
